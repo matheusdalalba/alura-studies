e094b57f32157096641d71efd9af940330ae33b8
Dal'Alba <dalalbaaa@gmail.com>
2023-08-17 08:56:26 -0300

    refactor: inicia estilização do componente Botao

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -2,7 +2,12 @@ import React from "react";
 
 class Botao extends React.Component {
   render() {
-    return <button>Botão</button>;
+    const estaAtivo = false;
+    const styles = {
+      backgroundColor: estaAtivo ? "green" : "red",
+    };
+
+    return <button style={styles}>Botão</button>;
   }
 }
 
